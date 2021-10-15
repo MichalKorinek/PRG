cd14a211ac50191802dfd3db43ea8e27c08c9b91
Michal <korinek.michal99@gmail.com>
2021-10-15 13:12:48 +0200

    first jsp

--- src/main/java/cz/uhk/ppro/inzeraty/InzeratyApplication.java
+++ src/main/java/cz/uhk/ppro/inzeraty/InzeratyApplication.java
@@ -2,12 +2,22 @@ package cz.uhk.ppro.inzeraty;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @SpringBootApplication
-public class InzeratyApplication {
-
+@ComponentScan({"cz.uhk.ppro.inzeraty.controller"})
+public class InzeratyApplication implements WebMvcConfigurer{
     public static void main(String[] args) {
         SpringApplication.run(InzeratyApplication.class, args);
     }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+    }
+
 
 }
---
+++ src/main/webapp/WEB-INF/jsp/index.jsp
@@ -0,0 +1,15 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Michal
+  Date: 15.10.2021
+  Time: 11:39
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+</body>
+</html>
